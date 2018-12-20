519c431f5b3e2dceb68be22c8ebfe706f9657bba
Yerko <yerkodezutter@gmail.com>
2018-12-20 11:53:51 +0100

    wheel color change

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ var topH = document.getElementById("topH");
 var midH = document.getElementById('mid');
 var botH = document.getElementById('bot');
 
-function MoveLayer() {
-  topH.style.color = "red";
+var contai = document.getElementById("container");
+
+contai.addEventListener("wheel", MoveLayer);
+
+function MoveLayer(evt) {
+  if(evt.deltaY >0){
+               topH.style.color = "red";
+               var colChange = setTimeout(grayC, 800);
+           } else {
+               topH.style.color = "blue";
+           }
+}
+
+function grayC(evt){
+  
 }
