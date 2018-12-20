1cfa6e6738d2f6279b4789b627e5b3298674557d
Yerko <yerkodezutter@gmail.com>
2018-12-20 13:30:32 +0100

    small change... not sure what

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var topH = document.getElementById("topH");
 var midH = document.getElementById('mid');
 var botH = document.getElementById('bot');
+var scrollE = thru;
 
 var contai = document.getElementById("container");
 
 contai.addEventListener("wheel", MoveLayer);
 
 function MoveLayer(evt) {
-  if (evt.deltaY > 0) {
-    topH.style.color = "red";
-    var colChange = setTimeout(grayC, 800);
+  if(scrollE == thru){
+    if (evt.deltaY > 0) {
+      topH.style.color = "red";
+      var colChange = setTimeout(grayC, 800);
+    } else {
+      topH.style.color = "blue";
+    }
   } else {
-    topH.style.color = "blue";
+
   }
 }
 
