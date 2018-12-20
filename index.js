483ae851929f3a2b798fa1906db0d012f7340779
Yerko <yerkodezutter@gmail.com>
2018-12-20 14:43:09 +0100

    spam black added

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var topH = document.getElementById("topH");
 var midH = document.getElementById('mid');
 var botH = document.getElementById('bot');
-var scrollE = thru;
+var scrollE = true;
 
 var contai = document.getElementById("container");
 
 contai.addEventListener("wheel", MoveLayer);
 
 function MoveLayer(evt) {
-  if(scrollE == thru){
+  if(scrollE == true){
     if (evt.deltaY > 0) {
       topH.style.color = "red";
       var colChange = setTimeout(grayC, 800);
+      scrollE = false;
     } else {
       topH.style.color = "blue";
+      var colChange = setTimeout(grayC, 800);
+      scrollE = false;
     }
   } else {
 
@@ -22,4 +25,5 @@ function MoveLayer(evt) {
 
 function grayC(evt) {
   topH.style.color = "gray";
+  scrollE = true;
 }
